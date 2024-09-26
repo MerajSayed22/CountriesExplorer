c2886ef21d757c55404afeb3889ecfd2014f288c
Meraj <mrj.ahmed22@gmail.com>
2024-09-26 23:34:05 +0530

    Added filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import SearchBar from "./components/SearchBar"
 import Header from "./components/Header"
+import SelectMenu from "./components/SelectMenu"
 import "./App.css"
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
     <Header componentText= {"Where in the world?"} componentTheme = {"Dark Theme"}/>
+    <div className="search-filter-container">
     <SearchBar placeholder = {"Search for a country..."}/>
+    <SelectMenu />
+    </div>
     </>
   )
 }
