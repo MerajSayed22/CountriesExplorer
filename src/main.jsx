922df0b353c9f501efab83bd0c7b6e1757ca4e77
Meraj <mrj.ahmed22@gmail.com>
2024-10-01 09:00:03 +0530

    Router Error Handling Created

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter,RouterProvider,} from "react-router-dom"
 import Home from './components/Home'
 import Contact  from './components/Contact'
+import Error from './components/Error'
 import App from './App'
 
 
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error />,
     children:[
       {
         path: "/",
